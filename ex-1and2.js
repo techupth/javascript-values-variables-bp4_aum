c585e3ed4ba0aed4b9a65d1476f0bec3d6acbb4c
Jirapat <jirapat.du24@gmail.com>
2024-05-26 18:38:33 +0700

    edit ex-1 and ex-2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,27 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
-let name = "Karter"
-console.log(name)
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-let lastName = "Anderson"
-console.log(lastName)
-
-const restaurantName = "Chef's Table by Chef James"
-console.log(restaurantName)
-
-
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const allRestaurantOpen = 1500;
 
 
 //Exercise #2: Variable Reassignment
 //Start coding here
-let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
 address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
-console.log (address)
-
-const totalPrice = 5000;
-console.log(totalPrice)
-
-const allRestaurantOpen = 2500;
-console.log(allRestaurantOpen)
-
+totalPrice = 5000;
+allRestaurantOpen = 2500;
 
