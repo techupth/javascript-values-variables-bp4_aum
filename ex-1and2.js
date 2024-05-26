1e99218b274886c624fe91acb31da14ebd7ae833
Jirapat <jirapat.du24@gmail.com>
2024-05-26 14:14:16 +0700

    ex-2

--- ex-1and2.js
+++ ex-1and2.js
@@ -6,22 +6,22 @@ console.log(name)
 let lastName = "Anderson"
 console.log(lastName)
 
-let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
-console.log (address)
-
 const restaurantName = "Chef's Table by Chef James"
 console.log(restaurantName)
-
-const totalPrice = 15000;
-console.log(totalPrice)
-
-const allRestaurantOpen = 1500;
-console.log(allRestaurantOpen)
 
 
 
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+console.log (address)
+
+const totalPrice = 5000;
+console.log(totalPrice)
+
+const allRestaurantOpen = 2500;
+console.log(allRestaurantOpen)
 
 
